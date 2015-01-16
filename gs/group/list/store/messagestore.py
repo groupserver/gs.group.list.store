85ceeff8c431e1ff4b68a291cd79319510ea0185
Michael JasonSmith <mpj17@onlinegroups.net>
2015-01-15 17:06:39 -0800

    Moving some methods back go gs.group.list.base

--- gs/group/list/store/messagestore.py
+++ gs/group/list/store/messagestore.py
@@ -59,88 +59,9 @@ class EmailMessageStore(EmailMessage):
         retval = FileMetadataStorageQuery(self)
         return retval
 
-    @staticmethod
-    def calculate_file_id(file_body, mime_type):
-        '''Generate a new identifer for a file
-:param bytes file_body: The body of the file
-:param string mime_type: The MIME-type of the file
-:returns: A 3-tuple of ``(identifier, length, fileMD5)
-
-Two files will have the same ID if
-
-* They have the same MD5 Sum, *and*
-* They have the same length, *and*
-* They have the same MIME-type.'''
-        length = len(file_body)
-        md5_sum = md5()
-        for c in file_body:
-            md5_sum.update(c)
-        file_md5 = md5_sum.hexdigest()
-        md5_sum.update(':' + str(length) + ':' + mime_type)
-        vNum = convert_int2b62(INT(md5_sum.hexdigest(), 16))
-        retval = (to_unicode_or_bust(vNum), length, file_md5)
-        return retval
-
     @Lazy
-    def attachments(self):
-        def split_multipart(msg, pl):
-            if msg.is_multipart():
-                for b in msg.get_payload():
-                    pl = split_multipart(b, pl)
-            else:
-                pl.append(msg)
-            return pl
-
-        retval = []
-        payload = self.message.get_payload()
-        if isinstance(payload, list):
-            outmessages = []
-            for i in payload:
-                outmessages = split_multipart(i, outmessages)
-
-            for msg in outmessages:
-                actual_payload = msg.get_payload(decode=True)
-                encoding = msg.get_param('charset', self.encoding)
-                pd = self.parse_disposition(msg.get('content-disposition',
-                                                    ''))
-                filename = to_unicode_or_bust(pd, encoding) if pd else ''
-                fileid, length, md5_sum = self.calculate_file_id(
-                    actual_payload, msg.get_content_type())
-                retval.append({
-                    'payload': actual_payload,
-                    'fileid': fileid,
-                    'filename': filename,
-                    'length': length,
-                    'md5': md5_sum,
-                    'charset': encoding,  # --=mpj17=-- Issues?
-                    'maintype': msg.get_content_maintype(),
-                    'subtype': msg.get_content_subtype(),
-                    'mimetype': msg.get_content_type(),
-                    'contentid': msg.get('content-id', '')})
-        else:
-            # Since we aren't a bunch of attachments, actually decode the
-            #   body
-            payload = self.message.get_payload(decode=True)
-            cd = self.message.get('content-disposition', '')
-            pd = self.parse_disposition(cd)
-            filename = to_unicode_or_bust(pd, self.encoding) if pd else ''
-
-            fileid, length, md5_sum = self.calculate_file_id(
-                payload, self.message.get_content_type())
-            retval = [{
-                      'payload': payload,
-                      'md5': md5_sum,
-                      'fileid': fileid,
-                      'filename': filename,
-                      'length': length,
-                      'charset': self.message.get_charset(),
-                      'maintype': self.message.get_content_maintype(),
-                      'subtype': self.message.get_content_subtype(),
-                      'mimetype': self.message.get_content_type(),
-                      'contentid': self.message.get('content-id', '')}]
-        assert retval is not None
-        assert type(retval) == list
-        return retval
+    def inreplyto(self):
+        return self.message.get('in-reply-to', '')
 
     @Lazy
     def attachment_count(self):
@@ -149,13 +70,6 @@ Two files will have the same ID if
             if item['filename']:
                 count += 1
         return count
-
-    @Lazy
-    def html_body(self):
-        for item in self.attachments:
-            if item['filename'] == '' and item['subtype'] == 'html':
-                return to_unicode_or_bust(item['payload'], self.encoding)
-        return ''
 
     @Lazy
     def date(self):
