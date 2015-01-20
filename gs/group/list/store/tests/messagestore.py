4588bc7eb45772ecb331bd2903ddc6b01893826d
Michael JasonSmith <mpj17@onlinegroups.net>
2015-01-20 09:34:50 -0800

    Testing the count of attachments

--- gs/group/list/store/tests/messagestore.py
+++ gs/group/list/store/tests/messagestore.py
@@ -13,10 +13,13 @@
 #
 ############################################################################
 from __future__ import absolute_import, unicode_literals
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from mock import (MagicMock)
 from unittest import TestCase
 from gs.group.list.base.emailmessage import EmailMessage
-from gs.group.list.store.messagestore import EmailMessageStore
+from gs.group.list.store.messagestore import (
+    EmailMessageStore)
 
 
 class EmailMessageStoreTest(TestCase):
@@ -43,3 +46,59 @@ Tonight on Ethel the Frog we look at violence.\n'''
         self.messageStore.message['In-reply-to'] = e
         r = self.messageStore.inreplyto
         self.assertEqual(e, r)
+
+    def test_attachment_count(self):
+        'Check that the text-body does not count as an attachment'
+        r = self.messageStore.attachment_count
+        self.assertEqual(0, r)
+
+    def get_txt_html_msg(self):
+        retval = MIMEMultipart()
+        a = MIMEMultipart('alternative')
+        retval.attach(a)
+        tt = MIMEText(
+            'Tonight on Ethel the Frog\u2026 we look at violence.\n',
+            'plain', 'UTF-8')
+        a .attach(tt)
+        th = MIMEText(
+            '<p>Tonight on Ethel the Frog&#8230; we look at '
+            'violence.\n</p>', 'html', 'us-ascii')
+        a.attach(th)
+
+        for h, v in self.messageStore.message.items():
+            retval.add_header(h, v)
+        return retval
+
+    def test_attachment_count_html_body(self):
+        self.messageStore.message = self.get_txt_html_msg()
+        r = self.messageStore.attachment_count
+        self.assertEqual(0, r)
+
+    def test_attachment_count_file(self):
+        'Can we see one file'
+        m = self.get_txt_html_msg()
+        textFile = MIMEText('The violence of British Gangland.')
+        textFile.add_header('Content-Disposition', 'attachment',
+                            filename='gangland.txt')
+        m.attach(textFile)
+        self.messageStore.message = m
+
+        r = self.messageStore.attachment_count
+        self.assertEqual(1, r)
+
+    def test_attachment_count_files(self):
+        'Can we see multiple files'
+        m = self.get_txt_html_msg()
+        textFile = MIMEText('The violence of British Gangland.')
+        textFile.add_header('Content-Disposition', 'attachment',
+                            filename='gangland.txt')
+        m.attach(textFile)
+        textFile = MIMEText('When he grew up he took to putting the boot '
+                            'in.')
+        textFile.add_header('Content-Disposition', 'attachment',
+                            filename='durk.txt')
+        m.attach(textFile)
+        self.messageStore.message = m
+
+        r = self.messageStore.attachment_count
+        self.assertEqual(2, r)
