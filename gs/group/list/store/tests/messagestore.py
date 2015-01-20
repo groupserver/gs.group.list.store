3c45302c216ce98b86aa5594ae476af29e0b18ed
Michael JasonSmith <mpj17@onlinegroups.net>
2015-01-20 10:42:12 -0800

    Testing EmailMessageStore.store_attachment

--- gs/group/list/store/tests/messagestore.py
+++ gs/group/list/store/tests/messagestore.py
@@ -15,17 +15,17 @@
 from __future__ import absolute_import, unicode_literals
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from mock import (MagicMock)
+from mock import (MagicMock, patch)
 from unittest import TestCase
 from gs.group.list.base.emailmessage import EmailMessage
-from gs.group.list.store.messagestore import (
-    EmailMessageStore)
+import gs.group.list.store.messagestore  # lint:ok
+from gs.group.list.store.messagestore import (EmailMessageStore)
 
 
 class EmailMessageStoreTest(TestCase):
     m = '''From: Me <a.member@example.com>
-To: Group <group@groups.example.com>
 Subject: Violence
+To: Group <group@groups.example.com>
 
 Tonight on Ethel the Frog we look at violence.\n'''
 
@@ -102,3 +102,88 @@ Tonight on Ethel the Frog we look at violence.\n'''
 
         r = self.messageStore.attachment_count
         self.assertEqual(2, r)
+
+    @staticmethod
+    def get_attachment(payload='', fileId='', filename='', length=0,
+                       md5_sum='', encoding='utf-8', mimetype='text/plain',
+                       cid=''):
+        maintype, subtype = mimetype.split('/')
+        retval = {
+            'payload': payload,
+            'fileid': fileId,
+            'filename': filename,
+            'length': length,
+            'md5': md5_sum,
+            'charset': encoding,
+            'maintype': maintype,
+            'subtype':  subtype,
+            'mimetype': mimetype,
+            'contentid': cid}
+        return retval
+
+    @patch('gs.group.list.store.messagestore.log')
+    def test_store_attachment_text_body(self, l):
+        'Ensure we do not store anything that looks like the text body'
+        p = 'Tonight on Ethel the Frog\u2026 we look at violence.\n'
+        a = self.get_attachment(payload=p.encode('utf-8'), length=len(p))
+
+        with patch.object(self.messageStore, 'add_file') as addFileMock:
+            self.messageStore.store_attachment(a)
+        self.assertEqual(0, addFileMock.call_count)
+
+    @patch('gs.group.list.store.messagestore.log')
+    def test_store_attachment_html_body(self, l):
+        'Ensure we do not store anything that looks like the HTML body'
+        p = '<p>Tonight on Ethel the Frog&#8230; we look at violence.</p>'
+        a = self.get_attachment(payload=p.encode('ascii'), length=len(p),
+                                mimetype='text/html')
+
+        with patch.object(self.messageStore, 'add_file') as addFileMock:
+            self.messageStore.store_attachment(a)
+        self.assertEqual(0, addFileMock.call_count)
+
+    @patch('gs.group.list.store.messagestore.log')
+    def test_store_attachment_cid_only(self, l):
+        '''Ensure we do not store anything that looks like a file that is
+just used in the HTML body'''
+        p = b'This is not an image'
+        a = self.get_attachment(payload=p, length=len(p),
+                                mimetype='image/jpeg', cid='something')
+
+        with patch.object(self.messageStore, 'add_file') as addFileMock:
+            self.messageStore.store_attachment(a)
+        self.assertEqual(0, addFileMock.call_count)
+
+    @patch('gs.group.list.store.messagestore.log')
+    def test_store_attachment_cid(self, l):
+        p = b'This is not an image'
+        a = self.get_attachment(payload=p, length=len(p),
+                                mimetype='image/jpeg', cid='something',
+                                filename="foo.jpg")
+
+        with patch.object(self.messageStore, 'add_file') as addFileMock:
+            self.messageStore.store_attachment(a)
+        self.assertEqual(1, addFileMock.call_count)
+
+    @patch('gs.group.list.store.messagestore.log')
+    def test_store_attachment_empty(self, l):
+        p = b''
+        a = self.get_attachment(payload=p, length=len(p),
+                                mimetype='image/jpeg', filename="foo.jpg")
+
+        with patch.object(self.messageStore, 'add_file') as addFileMock:
+            self.messageStore.store_attachment(a)
+        self.assertEqual(0, addFileMock.call_count)
+
+    @patch('gs.group.list.store.messagestore.log')
+    def test_store_attachment(self, l):
+        '''Ensure we do not store anything that looks like a file that is
+used in the HTML body'''
+        p = b'This is not an image'
+        a = self.get_attachment(payload=p, length=len(p),
+                                mimetype='image/jpeg',
+                                filename="foo.jpg")
+
+        with patch.object(self.messageStore, 'add_file') as addFileMock:
+            self.messageStore.store_attachment(a)
+        self.assertEqual(1, addFileMock.call_count)
